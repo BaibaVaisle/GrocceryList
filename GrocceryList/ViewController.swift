995f2d3d85b2d115cc822e99c69aab2f331423b7
Baiba <79092623+BaibaVaisle@users.noreply.github.com>
2021-08-04 19:00:51 +0300

    Initial Commit

---
+++ GrocceryList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrocceryList
+//
+//  Created by baiba.vaisle on 04/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
